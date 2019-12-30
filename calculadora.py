25b08c21de13fa4f22eff2e2fd0fc79899e69ac5
pedrob14 <pedrob14farias@gmail.com>
2019-12-30 13:00:28 -0300

    Até aula 6

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+class Calculadora:
+    area_paredes: float
+    area_teto: float
+
+    def calcular_area_paredes(self, largura, profundidade, altura):
+        self.area_paredes = 2 * (largura + profundidade) * altura
+        return self.area_paredes
+
+    def calcular_area_teto(self, largura, profundidade):
+        self.area_teto = largura * profundidade
+        return self.area_teto
+
+    def calcular_litragem_necessaria(self):
+        return (self.area_paredes + self.area_teto) / 10
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
+from calculadora import Calculadora
+
+calc = Calculadora()
+
 largura: float = float(input('Qual a largura do cômodo? '))
 profundidade: float = float(input('Qual a profundidade do cômodo?'))
 altura: float = 2.9
 
-area_paredes: float = 2 * (largura + profundidade) * altura
+print("A area das paredes é",
+      calc.calcular_area_paredes(largura, profundidade, altura))
 
-print("A area das paredes é", area_paredes)
 
-area_teto: float = largura * profundidade
 
 print(
     'A area do teto é:',
-    area_teto
-
+    calc.calcular_area_teto(largura, profundidade)
 )
 
 print('A litragem de tinta necesária é:',
-      (area_paredes + area_teto) / 10
+      calc.calcular_litragem_necessaria()
       )
