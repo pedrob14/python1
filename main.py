6cac875c26503632442eb3dec70ff0e1c04572e2
Pedro Henrique <46195037+pedrob14@users.noreply.github.com>
2019-12-31 19:30:09 -0300

    Curso introdutório concluído

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from calculadora import Calculadora
+from comodo import Comodo
 
 calc = Calculadora()
 
-largura: float = float(input('Qual a largura do cômodo? '))
-profundidade: float = float(input('Qual a profundidade do cômodo?'))
-altura: float = 2.9
-
-print("A area das paredes é",
-      calc.calcular_area_paredes(largura, profundidade, altura))
-
-
-
-print(
-    'A area do teto é:',
-    calc.calcular_area_teto(largura, profundidade)
+comodo = Comodo(
+    input('Qual a largura do cômodo? '),
+    input('Qual a profundidade do cômodo? ')
 )
 
-print('A litragem de tinta necesária é:',
-      calc.calcular_litragem_necessaria()
-      )
+print(
+    "A area das paredes é:",
+    calc.calcular_area_paredes(
+        comodo
+    )
+)
+
+print(
+    'A área do teto é:',
+    calc.calcular_area_teto(
+        comodo
+    )
+)
+
+print(
+    'A litragem de tinta necessária é:',
+    calc.calcular_litragem_necessaria()
+)
